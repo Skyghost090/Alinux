a2b0ddabef5dc442ef65c9fb762050991e8b5703
Skyghost090 <120269841+Skyghost090@users.noreply.github.com>
2025-11-14 12:30:00 -0300

    Fix && operator issue

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 pkg install proot-distro tur-repo -y
 proot-distro install ubuntu
-proot-distro login ubuntu -- apt update && apt install i3 fzf xterm -y
+proot-distro login ubuntu -- apt update
+proot-distro login ubuntu -- apt install i3 fzf xterm -y
 cp main.sh /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/
 mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.config/i3/
 cp -p config /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.config/i3/
